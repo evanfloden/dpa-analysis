73e4ec98489a5b8f2536f7f73abb5512456c5c8f
skptic <evanfloden@gmail.com>
2017-08-31 11:58:39 +0200

    Fixed MAFFT with custom trees

--- templates/std_align/std_align_MAFFT.sh
+++ templates/std_align/std_align_MAFFT.sh
@@ -1,2 +1,5 @@
 t_coffee -other_pg seq_reformat -in ${guide_tree} -input newick -in2 ${seqs} -input2 fasta_seq -action +newick2mafftnewick >> ${id}.mafftnewick
-mafft --anysymbol --treein ${id}.mafftnewick ${seqs} > ${id}.std.${align_method}.with.${tree_method}.tree.aln
+
+/mafft/mafft-7.310-with-extensions/core/newick2mafft.rb 1.0 ${id}.mafftnewick > ${id}.mafftbinary
+
+/mafft/bin/mafft --anysymbol --treein ${id}.mafftbinary ${seqs} > ${id}.std.${align_method}.with.${tree_method}.tree.aln
