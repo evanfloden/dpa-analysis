d7b61d43bf463d3478d7ad403d5715d94114597e
Edgar <edgano@gmail.com>
2017-10-16 13:46:02 +0200

    Create dpa_align_MSA.sh

---
+++ templates/dpa_align/dpa_align_MSA.sh
@@ -0,0 +1,4 @@
+t_coffee -dpa -dpa_method msa_msa \
+         -seq ${seqs} \
+         -dpa_nseq ${bucket_size} \
+         -outfile ${id}.dpa_${bucket_size}.${align_method}.with.${tree_method}.tree.aln 
