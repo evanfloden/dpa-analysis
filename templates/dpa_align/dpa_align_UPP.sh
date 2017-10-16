e683115eb25dd794f237752bda34a90e26eaa68c
skptic <evanfloden@gmail.com>
2017-10-16 13:42:03 +0200

    Added Mafft-Sparsecore

--- templates/dpa_align/dpa_align_UPP.sh
+++ templates/dpa_align/dpa_align_UPP.sh
@@ -2,4 +2,5 @@ t_coffee -dpa -dpa_method upp_msa \
          -dpa_tree ${guide_tree} \
          -seq ${seqs} \
          -dpa_nseq ${bucket_size} \
-         -outfile ${id}.dpa_${bucket_size}.${align_method}.with.${tree_method}.tree.aln
+         -outfile ${id}.dpa_${bucket_size}.${align_method}.with.${tree_method}.tree.aln \
+         -n_core=1
