ca9d840abde2599dea684b55fa2b882c25b52abb
skptic <evanfloden@gmail.com>
2018-09-25 15:02:56 +0200

    Reworked all notebooks

---
+++ templates/tree/.ipynb_checkpoints/generate_tree_MAFFT-FFTNS1-checkpoint.sh
@@ -0,0 +1,2 @@
+mafft-fftns --retree 1 --anysymbol --treeout ${seqs}
+t_coffee -other_pg seq_reformat -in ${seqs}.tree -in2 ${seqs} -input newick -action +mafftnewick2newick > ${id}.${tree_method}.dnd
