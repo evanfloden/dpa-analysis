a56b668a06c4eb4ac05b047148905319b6f2efe4
skptic <evanfloden@gmail.com>
2018-10-16 15:24:48 +0200

    added cpu calculation

--- bin/cpu_calculate.sh
+++ bin/cpu_calculate.sh
@@ -6,3 +6,5 @@ awk -F "--" '{print $7 > $2".std_align."$5"."$3"."$6".cpu"}'
 
 cat trace.txt | grep "default_alignment" | grep "COMPLETED\|CACHED" | cut -f 4,7 |sed 's/[()]//g' |awk -F " " '{print $1"--"$2"--"$3}'| awk -F "." '{print $1"--"$2"--"$3"--"$4"--"$5}'|\
 awk -F "--" '{print $7 > $2".default_align."$5"."$3"."$6".cpu"}'
+
+mv *.cpu $1/.
--- main.nf
+++ main.nf
@@ -105,7 +105,6 @@ if( params.refs ) {
 if ( params.trees ) {
   Channel
     .fromPath(params.trees)
-    .view()
     .map { item -> [ item.baseName.tokenize('.')[0], item.baseName.tokenize('.')[1], item] }
     .set { trees }
 }
